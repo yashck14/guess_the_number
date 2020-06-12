37e2c47dd2f525964c4e2fd4821f7dc719895411
yashck14 <64610666+yashck14@users.noreply.github.com>
2020-06-13 03:20:07 +0530

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,26 +1,40 @@
+# yash kushwaha
+# all errors are handled
 from random import randrange as r
-a = int(input("number :- "))
-b = int(input("number :- "))
+while True:
+	try:
+		a = int(input("enter first number :- \n"))
+		b = int(input("enter second number :- \n"))
+		break
+	except:
+		continue 
 chance = {}
+print("\nplayer1 chance\n")
 for i in range(1,3):
 	c = r(a,b)
 	chance["player{0}".format(i)] = 0
 	u = 0
 	while u != c:
-		u = int(input("your guess"))
+		while True:
+			try:
+				u = int(input("your guess "))
+				break
+			except:
+				continue	
 		if u in range(a,b):
 			if u == c:
 				print("you win\n")
 				if i == 1:
-					print(f"\nnow player{i} chance\n")
+					print(f"---------------\nnow player2 chance\n")
 			else:
-				print("try again")
+				print("try again\n")
 				chance["player{0}".format(i)] = chance["player{0}".format(i)] + 1
 		else:
-			print("your digit is not in range")
+			print("your digit is not in range\n")
 			
-
 if int(chance['player1']) > int(chance['player2']):
-	print("player 1 lose")
+	print("player 1 lose \nplayer 2 win")
+elif int(chance['player1']) == int(chance['player2']):
+	print("draw")
 else:
-	print("player 2 lose")
+	print("player 2 lose\nplayer 1 win")
